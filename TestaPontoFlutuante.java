c54cedcd9d4ea1b7f2ca01d4bd330d3aea464872
David Gonçalves <81859648+davg505@users.noreply.github.com>
2022-10-10 13:40:00 -0300

    Add files via upload

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,16 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario = 1270.5;
+		
+		System.out.println("Meu salario é " +  salario );
+		
+		double divisao = 5.0 / 2;
+		
+		System.out.println(divisao);
+
+	}
+
+}
---
+++ TestaVariaveis.java
@@ -0,0 +1,13 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Ola novo teste");
+		
+		int idade = 37;
+				
+		System.out.println("a idade é "+ idade + ", Parabens");
+
+	}
+
+}
